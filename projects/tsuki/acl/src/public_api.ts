e3a5d969f23ae72147428e224591e342d84ccf26
Tsukatsuki Kanade <i@sukitsuki.com>
2018-10-27 17:17:21 +0700

    rename Pack

--- projects/tsuki/acl/src/public_api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of acl
- */
-
-export * from './lib/acl.service';
-export * from './lib/acl.component';
-export * from './lib/acl.module';
---
+++ projects/tsuki/pack/src/public_api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of pack
+ */
+
+export * from './acl/acl.service';
+export * from './acl/acl.module';
